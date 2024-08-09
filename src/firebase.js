4a65e434a5390bb68e1749cc273454428c10b9a9
Md Asif Ahmad <114146519+Asif211026@users.noreply.github.com>
2024-08-09 23:14:43 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjDypmQr5cq0-uOeIP5IsiXXI8FskHG_Y",
+  authDomain: "ecommercereact-a3bee.firebaseapp.com",
+  projectId: "ecommercereact-a3bee",
+  storageBucket: "ecommercereact-a3bee.appspot.com",
+  messagingSenderId: "707609351938",
+  appId: "1:707609351938:web:77d0161f97b6785ed3541b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
